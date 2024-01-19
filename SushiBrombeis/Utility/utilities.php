6c7de369a5ea33f6f2271c1e2d5dde53ae601921
Barutta02 <88086997+Barutta02@users.noreply.github.com>
2024-01-19 18:52:03 +0100

    startClearing

--- SushiBrombeis/Utility/utilities.php
+++ SushiBrombeis/Utility/utilities.php
@@ -2,7 +2,7 @@
 
 require_once 'ViewsUtility.php';
 
-define("ROOT_FOLDER", "/TecWeb/Home/");
+define("ROOT_FOLDER", "/TecWeb/SushiBrombeis/");
 
 function render_page($html, $title, $id, $breadcrumbs, $keywords, $description, $content, $onload, )
 {
